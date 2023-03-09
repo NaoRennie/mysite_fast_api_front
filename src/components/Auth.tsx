6d716eb4703f3b3736e71641aa5f689880647ea9
NaoRennie <naomi.lily.405@gmail.com>
2023-03-09 11:07:19 +0900

    added css to components

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react'
 import { RefreshIcon } from '@heroicons/react/solid'
-import { BadgeCheckIcon } from '@heroicons/react/solid'
 import { useProcessAuth } from '../hooks/useProcessAuth'
 import './Auth.css'
 
@@ -25,7 +24,6 @@ export const Auth: FC = () => {
     processAuth,
     registerMutation,
     loginMutation,
-    // logout,
   } = useProcessAuth()
   if (registerMutation.isLoading || loginMutation.isLoading) {
     return (
@@ -36,112 +34,159 @@ export const Auth: FC = () => {
   }
   return (
     <div className="auth-content">
-      <div className="auth-badgeCheckIcon-content">
-        <BadgeCheckIcon className="auth-icon" />
-        <span className="auth-badgeCheckIcon-text">Todo web app</span>
+      <div className="auth-title-content">
+        <span className="auth-title-text">Todo web app</span>
       </div>
-      <h2 className="auth-label">
-        {isLogin ? 'Login' : 'Create a new account'}
-      </h2>
-      <form onSubmit={processAuth}>
-        <div>
-          <input
-            className="auth-input"
-            name="username"
-            type="text"
-            autoFocus
-            placeholder="user name"
-            onChange={(e) => setUsername(e.target.value)}
-            value={username}
-          />
-        </div>
-        <div>
-          <input
-            className="auth-input"
-            name="email"
-            type="email"
-            autoFocus
-            placeholder="Email address"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </div>
-        <div>
-          <input
-            className="auth-input"
-            name="password"
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPw(e.target.value)}
-            value={pw}
-          />
-        </div>
-        <div>
-          <input
-            className="auth-input"
-            name="birthday"
-            type="text"
-            autoFocus
-            placeholder="birthday(2022/1/1)"
-            onChange={(e) => setBirthday(e.target.value)}
-            value={birthday}
-          />
-        </div>
-        <div>
-          <input
-            className="auth-input"
-            name="gender"
-            type="text"
-            autoFocus
-            placeholder="gender"
-            onChange={(e) => setGender(e.target.value)}
-            value={gender}
-          />
-        </div>
-        <div>
-          <p className="auth-input-p">アバターファイル選択</p>
-          <input
-            className="auth-input"
-            name="avatar"
-            type="file"
-            accept="image/*"
-            autoFocus
-            placeholder="avatar"
-            onChange={(e) => setAvatar(e.target.value)}
-            value={avatar}
-          />
-        </div>
+      {isLogin ? (
+        <form className="auth-form" onSubmit={processAuth}>
+          <h2>login</h2>
+          <div>
+            <input
+              className="auth-input"
+              name="username"
+              type="text"
+              autoFocus
+              placeholder="user name"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="email"
+              type="email"
+              autoFocus
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPw(e.target.value)}
+              value={pw}
+            />
+          </div>
+          <div>
+            <button
+              className="auth-button"
+              disabled={!email || !pw || !username}
+              type="submit"
+            >
+              {isLogin ? 'Login' : 'Sign Up'}
+            </button>
+          </div>
+        </form>
+      ) : (
+        <form className="auth-form" onSubmit={processAuth}>
+          <h2>create a new account</h2>
+          <div>
+            <input
+              className="auth-input"
+              name="username"
+              type="text"
+              autoFocus
+              placeholder="user name"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="email"
+              type="email"
+              autoFocus
+              placeholder="Email address"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="password"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPw(e.target.value)}
+              value={pw}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="birthday"
+              type="text"
+              autoFocus
+              placeholder="birthday(2022/1/1)"
+              onChange={(e) => setBirthday(e.target.value)}
+              value={birthday}
+            />
+          </div>
+          <div>
+            <input
+              className="auth-input"
+              name="gender"
+              type="text"
+              autoFocus
+              placeholder="gender"
+              onChange={(e) => setGender(e.target.value)}
+              value={gender}
+            />
+          </div>
+          <div>
+            <p className="auth-input-p">アバターファイル選択</p>
+            <input
+              className="auth-input"
+              name="avatar"
+              type="file"
+              accept="image/*"
+              autoFocus
+              placeholder="avatar"
+              onChange={(e) => setAvatar(e.target.value)}
+              value={avatar}
+            />
+          </div>
 
-        <div>
-          <p className="auth-input-p">利用規約を読みましたか？</p>
-          <label>承諾します</label>
-          <input
-            className="auth-input"
-            name="afarmitive"
-            type="radio"
-            autoFocus
-            placeholder="yes"
-            onChange={(e) => setAgreement(true)}
-          />
-        </div>
-        <div>
-          <button
-            className="auth-button"
-            disabled={
-              !email ||
-              !pw ||
-              !username ||
-              !birthday ||
-              !gender ||
-              !avatar ||
-              !agreement
-            }
-            type="submit"
-          >
-            {isLogin ? 'Login' : 'Sign Up'}
-          </button>
-        </div>
-      </form>
+          <div>
+            <p className="auth-input-p">利用規約を読みましたか？</p>
+            <label>承諾します</label>
+            <input
+              className="auth-input"
+              name="afarmitive"
+              type="radio"
+              autoFocus
+              placeholder="yes"
+              onChange={(e) => setAgreement(true)}
+            />
+          </div>
+          <div>
+            <button
+              className="auth-button"
+              disabled={
+                !email ||
+                !pw ||
+                !username ||
+                !birthday ||
+                !gender ||
+                !avatar ||
+                !agreement
+              }
+              type="submit"
+            >
+              {isLogin ? 'Login' : 'Sign Up'}
+            </button>
+          </div>
+        </form>
+      )}
+      <p className="toggle-p">
+        {isLogin ? '新しくアカウントを作成する' : 'ログインする'}
+      </p>
       <RefreshIcon onClick={() => setIsLogin(!isLogin)} className="auth-icon" />
     </div>
   )
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,32 +1,102 @@
-import { FC } from 'react'
-import { LogoutIcon } from '@heroicons/react/solid'
-import { ShieldCheckIcon } from '@heroicons/react/outline'
+import { FC, useState } from 'react'
+import { LogoutIcon } from '@heroicons/react/outline'
+import { ShieldCheckIcon } from '@heroicons/react/solid'
 import { useAppSelector, useAppDispatch } from '../app/hooks'
 import { setEditedTask, selectTask } from '../slices/appSlice'
 import { useProcessAuth } from '../hooks/useProcessAuth'
 import { useProcessTask } from '../hooks/useProcessTask'
 import { useQueryTasks } from '../hooks/useQueryTasks'
 import { useQueryUser } from '../hooks/useQueryUser'
+import { useQuerySingleTask } from '../hooks/useQuerySingleTask'
 import { TaskItem } from './TaskItem'
 import './Todo.css'
 
 export const Todo: FC = () => {
+  const [id, setId] = useState('')
   const { logout } = useProcessAuth()
   const { data: dataUser } = useQueryUser()
   const { data: dataTasks, isLoading: isLoadingTasks } = useQueryTasks()
+  const { data: dataSingleTask, isLoading: isLoadingTask } =
+    useQuerySingleTask(id)
+  const { processTask } = useProcessTask()
   const dispatch = useAppDispatch()
   const editedTask = useAppSelector(selectTask)
+
+  console.log(dataUser, 'this is userdate')
   return (
     <div className="todo-content">
       <div className="todo-title">
         <ShieldCheckIcon className="todo-icon" />
-        <span className="text-center text-3xl font-extrabold">Todo tasks</span>
+        <span className="text-center text-3xl font-extrabold">
+          {dataUser?.username}'s Todo Tasks
+        </span>
       </div>
-      <p className="todo-user-info">{dataUser?.username}</p>
-      <p className="todo-user-info">{dataUser?.birthday}</p>
-      <img src={dataUser?.avatar} alt={dataUser?.username} />
-
-      <LogoutIcon onClick={logout} className="todo-icon" />
+      <div className="todo-user-info">
+        <img src={dataUser?.avatar} alt={dataUser?.username} />
+        <p>
+          <span>user name:</span>
+          {dataUser?.username}
+        </p>
+        <p>
+          <span>birthday:</span>
+          {dataUser?.birthday}
+        </p>
+        <LogoutIcon onClick={logout} className="todo-icon" />
+      </div>
+      <form onSubmit={processTask}>
+        <input
+          className="todo-input"
+          placeholder="title ?"
+          type="text"
+          onChange={(e) =>
+            dispatch(setEditedTask({ ...editedTask, title: e.target.value }))
+          }
+          value={editedTask.title}
+        />
+        <input
+          className="todo-input"
+          placeholder="description ?"
+          type="text"
+          onChange={(e) =>
+            dispatch(
+              setEditedTask({ ...editedTask, description: e.target.value })
+            )
+          }
+          value={editedTask.description}
+        />
+        <button
+          className="todo-button"
+          disabled={!editedTask.title || !editedTask.description}
+        >
+          {editedTask.id === '' ? 'Create' : 'Update'}
+        </button>
+      </form>
+      <div className="todo-list">
+        {isLoadingTasks ? (
+          <p>Loading...</p>
+        ) : (
+          <div>
+            <p className="todo-title-text">Todo List</p>
+            <ul className="todo-ul">
+              {dataTasks?.map((task) => (
+                <TaskItem
+                  key={task.id}
+                  id={task.id}
+                  title={task.title}
+                  description={task.description}
+                  setId={setId}
+                />
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+      <h2 className="todo-title-text">Selected Task</h2>
+      {isLoadingTask && <p>Loading...</p>}
+      <div className="kakomi-tab2">
+        <p className="todo-p-text title-tab2">{dataSingleTask?.title}</p>
+        <p className="todo-p-text">{dataSingleTask?.description}</p>
+      </div>
     </div>
   )
 }
